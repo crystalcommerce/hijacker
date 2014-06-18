ef6923dec3a5730c35709d850ddb6205a3cd6006
Michael Xavier <michael@michaelxavier.net>
2014-06-18 09:26:43 -0700

    Add custom invalid database handler
    This will be necessary to display our not found page like we do in the
    controller methods version

--- lib/hijacker/middleware.rb
+++ lib/hijacker/middleware.rb
@@ -1,18 +1,33 @@
 module Hijacker
   class Middleware
-    def initialize(app)
+    HEADER_KEY = "HTTP_X_HIJACKER_DB".freeze
+    DEFAULT_NOT_FOUND = ->(env) {
+      [404, {}, ["Database #{get_database(env)} not found"]]
+    }
+
+    attr_reader :not_found
+
+    def initialize(app, options = {})
       @app = app
+      @not_found = options.fetch(:not_found, DEFAULT_NOT_FOUND)
     end
 
     def call(env)
-      if env['HTTP_X_HIJACKER_DB'].present?
+      if env[HEADER_KEY].present?
         begin
-          Hijacker.connect(env['HTTP_X_HIJACKER_DB'])
-        rescue Hijacker::InvalidDatabase => e
-          return [404, {}, ""]
+          Hijacker.connect(self.class.get_database(env))
+        rescue Hijacker::InvalidDatabase
+          return not_found.call(env)
         end
       end
+
       @app.call(env)
+    end
+
+  private
+
+    def self.get_database(env)
+      env[HEADER_KEY]
     end
   end
 end
--- spec/hijacker/middleware_spec.rb
+++ spec/hijacker/middleware_spec.rb
@@ -14,11 +14,22 @@ module Hijacker
       end
     end
 
+    before(:each) do
+      Hijacker.stub(:connect)
+    end
+
     describe "#call" do
       context "When the 'X-Hijacker-DB' header is set" do
         it "connects to the database from the header" do
           Hijacker.should_receive(:connect).with("sample-db")
-          get '/',{}, 'HTTP_X_HIJACKER_DB' => 'sample-db'
+          get '/', {}, 'HTTP_X_HIJACKER_DB' => 'sample-db'
+        end
+
+        it "passes through" do
+          resp = get '/', {}, "x-not-db-header" => "something"
+          expect(resp.status).to eq(200)
+          expect(resp.headers['blah']).to eq("blah")
+          expect(resp.body).to eq("success")
         end
       end
 
@@ -26,6 +37,33 @@ module Hijacker
         it "doesn't connect to any database" do
           Hijacker.should_not_receive(:connect)
           get '/',{}, "x-not-db-header" => "something"
+        end
+      end
+
+      context "database not found" do
+        before(:each) do
+          Hijacker.stub(:connect).and_raise(Hijacker::InvalidDatabase)
+        end
+
+        it "returns a 404" do
+          resp = get '/', {}, "HTTP_X_HIJACKER_DB" => "something"
+          expect(resp.status).to eq(404)
+          expect(resp.body).to eq("Database something not found")
+        end
+
+        context "custom missing database handler" do
+          def app
+            Rack::Builder.new do
+              use Hijacker::Middleware, :not_found => ->(env) { [404, {}, "You done goofed"]}
+              run lambda { |env| [200, { 'blah' => 'blah' }, ["success"]] }
+            end
+          end
+
+          it "uses the custom not found handler" do
+            resp = get '/', {}, "HTTP_X_HIJACKER_DB" => "something"
+            expect(resp.status).to eq(404)
+            expect(resp.body).to eq("You done goofed")
+          end
         end
       end
     end
