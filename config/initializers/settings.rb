17f937ba9169cf112f011678de0b6c8e17c2f236
Tom Brooks <d3vkit@gmail.com>
2019-05-06 16:41:56 -0700

    Revert "48 upgrade redis"

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -8,11 +8,12 @@ module Hijacker
         else
           ENV.fetch('RAILS_ENV', 'development')
         end
-
-  APP_CONFIG = YAML.load_file(path)['defaults'].with_indifferent_access
+  
+  APP_CONFIG = YAML.load_file(path)[env].with_indifferent_access
 
   APP_CONFIG.each_pair do |key, value|
     key.freeze
     value.freeze
   end.freeze
 end
+
