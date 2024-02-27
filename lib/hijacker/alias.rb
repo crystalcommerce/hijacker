1865505f4ef50ab22c57a0de20da48565a9b2a2c
Wen Luo <wen.luo@crystalcommerce.com>
2024-02-26 21:56:59 -0800

    added primary key and attr_accessible

--- lib/hijacker/alias.rb
+++ lib/hijacker/alias.rb
@@ -1,5 +1,8 @@
 module Hijacker
   class Alias < BaseModel
+    self.primary_key = :id
+    attr_accessible :database_id, :name
+    
     belongs_to :database, :class_name => "Hijacker::Database"
   end
 end
--- lib/hijacker/host.rb
+++ lib/hijacker/host.rb
@@ -1,4 +1,6 @@
 class Hijacker::Host < Hijacker::BaseModel
+  self.primary_key = :id
+  attr_accessible :hostname, :common_hostname, :port, :slave_id, :instance_name
 
   validates_format_of :hostname, :with => /^(#{URI::REGEXP::PATTERN::HOST}|#{URI::REGEXP::PATTERN::IPV6ADDR})$/
   belongs_to :slave, class_name: "Hijacker::Host"
